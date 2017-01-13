9c4c65f889c844f2e177905df7a080f762eb861f
Jens Neuhalfen <jens@neuhalfen.name>
2017-01-13 21:56:23 +0100

    Add testdata files

---
+++ demo_reencrypt.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+#
+# decrypts an encrypted ZIP file and reencrypts each file in the zip
+#
+
+ASSEMBLY=bouncy-castle-examples-1.0.2
+LOCATION=./build
+
+DEST=/tmp/gpg-example-$$
+
+[ -d "${DEST}" ] && rm -rf "${DEST}" 
+mkdir $DEST
+
+[ -f ./build/libs/${ASSEMBLY}.jar ] ||  ./gradlew installDist
+
+CP=${LOCATION}/libs/${ASSEMBLY}.jar
+for JAR in ${LOCATION}/install/bouncy-castle-examples/lib/*.jar
+do
+   CP=${CP}:${JAR}
+done
+
+java -cp ${CP} \
+   name.neuhalfen.projects.crypto.bouncycastle.examples.openpgp.MainExplodedSinglethreaded \
+   sender@example.com \
+   recipient@example.com \
+   ./src/test/resources/recipient.gpg.d/pubring.gpg  \
+   ./src/test/resources/recipient.gpg.d/secring.gpg recipient \
+   "src/test/resources/testdata//zip_encrypted_armor_signed.zip.gpg" "${DEST}"
+
+
+echo
+echo Results in $DEST
--- src/test/resources/testdata/create_testdata.sh
+++ src/test/resources/testdata/create_testdata.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-[ -f zip_encrypted_armored_not_signed.zip.gpg ] && rm zip_encrypted_armored_not_signed.zip.gpg
-zip - -r ../../../main |  gpg --batch --yes --no-default-keyring --homedir ../sender.gpg.d -e -a -r recipient@example.com -o zip_encrypted_armored_not_signed.zip.gpg
+[ -f zip_encrypted_armored_unsigned.zip.gpg ] && rm zip_encrypted_armored_unsigned.zip.gpg
+zip - -r ../../../main |  gpg --batch --yes --no-default-keyring --homedir ../sender.gpg.d -e -a -r recipient@example.com -o zip_encrypted_armored_unsigned.zip.gpg
 
 ##
 
-[ -f zip_encrypted_binary_not_signed.zip.gpg ] && rm zip_encrypted_binary_not_signed.zip.gpg
-zip - -r ../../../main |  gpg --batch --yes --no-default-keyring --homedir ../sender.gpg.d -e -r recipient@example.com -o zip_encrypted_binary_not_signed.zip.gpg
+[ -f zip_encrypted_binary_unsigned.zip.gpg ] && rm zip_encrypted_binary_unsigned.zip.gpg
+zip - -r ../../../main |  gpg --batch --yes --no-default-keyring --homedir ../sender.gpg.d -e -r recipient@example.com -o zip_encrypted_binary_unsigned.zip.gpg
 
 [ -f zip_encrypted_binary_signed.zip.gpg ] && rm zip_encrypted_binary_signed.zip.gpg
-zip -  -r ../../../main | echo sender |  gpg  --passphrase-fd <(echo sender) --yes --batch --yes --no-default-keyring --homedir ../sender.gpg.d -e -s -r recipient@example.com -o zip_encrypted_binary_signed.zip.gpg
+zip -  -r ../../../main |   gpg  --passphrase sender --yes --batch --yes --no-default-keyring --homedir ../sender.gpg.d -e -s -r recipient@example.com -o zip_encrypted_binary_signed.zip.gpg
 
 [ -f zip_encrypted_armor_signed.zip.gpg ] && rm zip_encrypted_armor_signed.zip.gpg
-zip -  -r ../../../main | echo sender |  gpg  --passphrase-fd <(echo sender) --yes --batch --yes --no-default-keyring --homedir ../sender.gpg.d -e -a -s -r recipient@example.com -o zip_encrypted_armor_signed.zip.gpg
+zip -  -r ../../../main |   gpg  --passphrase sender --yes --batch --yes --no-default-keyring --homedir ../sender.gpg.d -e -a -s -r recipient@example.com -o zip_encrypted_armor_signed.zip.gpg
