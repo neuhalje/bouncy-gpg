581f0589ded51114c19fb5ab08429f61853d77d3
Jens Neuhalfen <jens@neuhalfen.name>
2017-02-10 19:10:19 +0100

    Mark hash algorithms that are known to be broken/weak

--- src/main/java/name/neuhalfen/projects/crypto/bouncycastle/openpgp/algorithms/PGPHashAlgorithms.java
+++ src/main/java/name/neuhalfen/projects/crypto/bouncycastle/openpgp/algorithms/PGPHashAlgorithms.java
@@ -4,17 +4,25 @@ import org.bouncycastle.bcpg.HashAlgorithmTags;
 
 
 public enum PGPHashAlgorithms {
-    MD5(HashAlgorithmTags.MD5),
-    SHA1(HashAlgorithmTags.SHA1),
+    MD5(HashAlgorithmTags.MD5, true),
+    SHA1(HashAlgorithmTags.SHA1, true),
     SHA_224(HashAlgorithmTags.SHA224),
     SHA_256(HashAlgorithmTags.SHA256),
     SHA_384(HashAlgorithmTags.SHA384),
     SHA_512(HashAlgorithmTags.SHA512),
     RIPEMD160(HashAlgorithmTags.RIPEMD160),
     TIGER_192(HashAlgorithmTags.TIGER_192),
-    HAVAL_5_160(HashAlgorithmTags.HAVAL_5_160),;
+    HAVAL_5_160(HashAlgorithmTags.HAVAL_5_160, true);
 
     public final int id;
+
+    /**
+     * Is this algorithm KNOWN to be broken or are there any known attacks on it?
+     * <p>
+     * DO NOT TRUST THIS JUDGEMENT!
+     * <p>
+     * A value of 'false' does not guarantee, that the algorithm is safe!
+     */
     public final boolean insecure;
 
     PGPHashAlgorithms(int id) {
