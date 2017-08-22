1267069ec9a6e7a9c3ada59862f7a3d4f962cdfa
Jens Neuhalfen <jens@neuhalfen.name>
2017-08-22 09:20:08 +0200

    Prevent exposing internal passphrase array instance

--- src/main/java/name/neuhalfen/projects/crypto/bouncycastle/openpgp/keys/callbacks/StaticPasswordKeyringConfigCallback.java
+++ src/main/java/name/neuhalfen/projects/crypto/bouncycastle/openpgp/keys/callbacks/StaticPasswordKeyringConfigCallback.java
@@ -1,17 +1,19 @@
 package name.neuhalfen.projects.crypto.bouncycastle.openpgp.keys.callbacks;
 
 
+import org.bouncycastle.util.Arrays;
+
 final class StaticPasswordKeyringConfigCallback implements KeyringConfigCallback {
 
 
   private final char[] passphrase;
 
   public StaticPasswordKeyringConfigCallback(char[] passphrase) {
-    this.passphrase = passphrase;
+    this.passphrase = Arrays.clone(passphrase);
   }
 
   @Override
   public char[] decryptionSecretKeyPassphraseForSecretKeyId(long keyID) {
-    return passphrase;
+    return  Arrays.clone(passphrase);
   }
 }
